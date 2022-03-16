c095f708d64d7439932670e7b8deec3fc81c2d08
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-16 11:14:02 -0400

    jenkins updates

--- Project2.sql
+++ Project2.sql
@@ -1,4 +1,9 @@
 
+-- creates DB if doesnt exists
+select 'Create Database Project2'
+where not exists (select from pg_database where datname = 'Project2');
+
+
 
 --drops procedures
 drop procedure if exists  approve_request;
@@ -64,7 +69,7 @@ Create Table employee_permissions(
 	employee_permission_id 	Integer generated always as identity,
 	employee_id 			Integer not null,
 	permission_id			Integer not null, -- id kept 
-	permission_type			varchar(30) -- redundant, here for ORM 
+	permission_type			varchar(30), -- redundant, here for ORM 
 	primary key(employee_permission_id),
 	Constraint fk_employees_p_employees Foreign Key(employee_id) References employees(employee_id),
 	Constraint fk_employees_p_permissions Foreign Key(permission_id) References permissions(permission_id)
@@ -156,16 +161,16 @@ insert into employee_permissions(employee_id, permission_id,permission_type)
 			(2,0,'standard employee'),
 			(3,0,'standard employee');
 	
-insert into reimbursements(employee_id,status,reimbursement_type,date_of_transaction,amount,details,merchant)
-	values	(2,'Approved','travel','2020-01-10',150,'hotel','mariot'),
-			(2,'Approved','travel','2020-01-10',30,'gas','speedway'),
-			(2,'Denied','travel','2020-01-10',300,'fancy restraunt','food place'),
-			(1,'Approved',4,'2020-02-25',1200,'ohh new machinery','Bobs hardware'),
-			(1,'New',5,'2020-02-26',3000,'worker injury compensation hospital visit','hospital'),
-			(3,'New',3,'2020-02-27',140,'replace broken chair','furniture place');
+insert into reimbursements(employee_id,status,reimbursement_type,date_of_transaction,amount,details,merchant,date_of_submission)
+	values	(2,'Approved','travel','2020-01-10',150,'hotel','mariot','2020-01-14'),
+			(2,'Approved','travel','2020-01-10',30,'gas','speedway','2020-01-14'),
+			(2,'Denied','travel','2020-01-10',300,'fancy restraunt','food place','2020-01-14'),
+			(1,'Approved',4,'2020-02-25',1200,'ohh new machinery','Bobs hardware','2020-02-25'),
+			(1,'New',5,'2020-02-26',3000,'worker injury compensation hospital visit','hospital','2020-02-26'),
+			(3,'New',3,'2020-02-27',140,'replace broken chair','furniture place','2020-02-28');
 
 
-insert into reimbursement_updates(reimbursement_id,date_of_update,update_comment)
+insert into reimbursement_updates(reimbursement_id,status,date_of_update,update_comment)
 	values	(1,'New','2020-01-14','reimbursement request submitted'),   --comment text can be changed for default comment
 			(2,'New','2020-01-14','reimbursement request submitted'),
 			(3,'New','2020-01-14','reimbursement request submitted'),
@@ -178,42 +183,43 @@ insert into reimbursement_updates(reimbursement_id,date_of_update,update_comment
 			(6,'New','2020-02-27','reimbursement request submitted');
 
 
+-- procedure and views comment out for ORM for now
 
 -------------------------------------------------
 -- Views
 -------------------------------------------------
 -- gets all reimbursements and all relevent information from them.
-create view all_reimbursements(db_reimbursement_id,db_status_id,db_employee_id,employee,status,r_type,amount,details,current_comment,date_of_transaction,date_submited,date_of_update,merchant)
-	as select 
-		reimbursements.reimbursement_id as db_reimbursement_id, 
-		reimbursements.status_id	as db_status_id,
-		employees.employee_id as db_employee_id,
-		concat(employees.first_name, ' ',employees.last_name) as employee,
-		reimbursement_statuss.status as status,
-		reimbursement_types.reimbursement_type as r_type,
-		reimbursements.amount as amount,
-		reimbursements.details as details,
-		current_update.update_comment as current_comment,
-		reimbursements.date_of_transaction as date_of_transaction,
-		initial_update.date_of_update as date_submitted,
-		current_update.date_of_update as date_of_update,
-		reimbursements.merchant
-	from reimbursements
-	inner join reimbursement_statuss 
-	on reimbursements.status_id = reimbursement_statuss.status_id
-	inner join reimbursement_types
-	on reimbursements.type_id = reimbursement_types.type_id
-	inner join employees 
-	on reimbursements.employee_id = employees.employee_id
-	inner join reimbursement_updates as current_update
-	on	reimbursements.reimbursement_id = current_update.reimbursement_id 
-	and reimbursements.status_id = current_update.status_id
-	inner join reimbursement_updates as initial_update
-	on	reimbursements.reimbursement_id = initial_update.reimbursement_id 
-	and initial_update.status_id = 1;
+--create view all_reimbursements(db_reimbursement_id,db_status_id,db_employee_id,employee,status,r_type,amount,details,current_comment,date_of_transaction,date_submited,date_of_update,merchant)
+--	as select 
+--		reimbursements.reimbursement_id as db_reimbursement_id, 
+--		reimbursements.status_id	as db_status_id,
+--		employees.employee_id as db_employee_id,
+--		concat(employees.first_name, ' ',employees.last_name) as employee,
+--		reimbursement_statuss.status as status,
+--		reimbursement_types.reimbursement_type as r_type,
+--		reimbursements.amount as amount,
+--		reimbursements.details as details,
+--		current_update.update_comment as current_comment,
+--		reimbursements.date_of_transaction as date_of_transaction,
+--		initial_update.date_of_update as date_submitted,
+--		current_update.date_of_update as date_of_update,
+--		reimbursements.merchant
+--	from reimbursements
+--	inner join reimbursement_statuss 
+--	on reimbursements.status_id = reimbursement_statuss.status_id
+--	inner join reimbursement_types
+--	on reimbursements.type_id = reimbursement_types.type_id
+--	inner join employees 
+--	on reimbursements.employee_id = employees.employee_id
+--	inner join reimbursement_updates as current_update
+--	on	reimbursements.reimbursement_id = current_update.reimbursement_id 
+--	and reimbursements.status_id = current_update.status_id
+--	inner join reimbursement_updates as initial_update
+--	on	reimbursements.reimbursement_id = initial_update.reimbursement_id 
+--	and initial_update.status_id = 1;
 	
 	-- view for viewing employees
-create view v_employees(employee_id,full_name,email,phone,job_title)
+/*create view v_employees(employee_id,full_name,email,phone,job_title)
 	as select employee_id, 
 	concat(employees.first_name, ' ',employees.last_name) as full_name, 
 	email,
@@ -222,16 +228,16 @@ create view v_employees(employee_id,full_name,email,phone,job_title)
 	from employees
 	inner join job_titles
 	on employees.job_title_id = job_titles.job_title_id;
-	
-create view v_employee_permissions(employee_id,permission_id,permission_type)
+*/	
+/*create view v_employee_permissions(employee_id,permission_id,permission_type)
 	as select employee_id, permissions.permission_id, permission_type
 	from permissions
 	inner join employee_permissions
 	on permissions.permission_id = employee_permissions.permission_id;
 	
-	
+*/	
 	-- view for login relevant info
-create view v_employee_login(employee_id,full_name,email,phone,job_title,first_name,last_name,user_name,user_password)
+/*create view v_employee_login(employee_id,full_name,email,phone,job_title,first_name,last_name,user_name,user_password)
 	as select employee_id, 
 	concat(employees.first_name, ' ',employees.last_name) as full_name, 
 	email,
@@ -245,13 +251,13 @@ create view v_employee_login(employee_id,full_name,email,phone,job_title,first_n
 	inner join job_titles
 	on employees.job_title_id = job_titles.job_title_id;
 
-	
+	*/
 
 -------------------------------------------------
 -- procedures
 -------------------------------------------------	
 	
-
+/*
 CREATE PROCEDURE approve_request(IN reim_id integer, IN stat_id integer, IN com varchar(200))
 LANGUAGE 'sql'
 AS $BODY$
@@ -264,7 +270,9 @@ $BODY$;
 ALTER PROCEDURE public.approve_request(integer, integer, character varying)
     OWNER TO postgres;
 
+*/
 
+/*
 CREATE PROCEDURE employee_login(IN u_name varchar(30), 
 								IN u_password varchar(30), 
 								out  first_name varchar(30),
@@ -290,7 +298,7 @@ $BODY$;
 ALTER PROCEDURE public.approve_request(integer, integer, character varying)
     OWNER TO postgres;
 
-
+*/
 
 
 
--- dockerfile
+++ dockerfile
@@ -1,3 +1,3 @@
-FROM openjdk:8-jdk-alpine
+FROM openjdk:latest
 COPY target/Project2Backend-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom -Djava.net.preferIPv4Stack=true","-jar","/app.jar"]
