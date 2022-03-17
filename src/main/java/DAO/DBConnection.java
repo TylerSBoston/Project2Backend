fa73bf9b31ba872f01f8ff7d32573d43fb345fba
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-17 13:04:14 -0400

    didn't save connection string change correcltly last time

--- src/main/java/DAO/DBConnection.java
+++ src/main/java/DAO/DBConnection.java
@@ -13,7 +13,7 @@ public class DBConnection {
 	private static Connection conn = null;
 	private final static Logger log = LogManager.getLogger(DBConnection.class);
 	private static String localConnectionString = "jdbc:postgresql://localhost:5432/Project1";
-	private static String ec2ConnectionString = "jdbc:postgresql:ip-172-31-13-88.us-east-2.compute.internal:8888/Project2";
+	private static String ec2ConnectionString = "jdbc:postgresql://ip-172-31-13-88.us-east-2.compute.internal:8888/Project2";
 	
 	private static String localPassword = "1234";
 	private static String ec2Password = "mysecretpassword";
@@ -31,8 +31,9 @@ public class DBConnection {
 			log.info("Estabilishing DB connection");
 			conn = DriverManager.getConnection(connectionString,userName,password);
 		} catch (SQLException e) {
+			// temporary remove backup connection
 			//
-			try
+	/*		try
 			{
 				// Dimitri, place your DB password and connection string here so the backend will also work for on your side
 				password = "Samara-13";
@@ -40,9 +41,10 @@ public class DBConnection {
 				conn = DriverManager.getConnection(connectionString,userName,password);
 			}
 			catch(SQLException e2)
-			{
-				log.error("Connection Failed " + e2.getMessage());
-			}
+			{ */
+				log.error("Connection Failed " + e.getMessage());
+				e.printStackTrace();
+		//	}
 		}
 	}
 	
