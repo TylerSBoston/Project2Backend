f0aa1ee1c30df6f2a0bfc86374c5a92b939901a6
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-24 13:55:55 -0400

    transactional fix?

--- src/main/java/com/Project2Backend/dao/ImageDao.java
+++ src/main/java/com/Project2Backend/dao/ImageDao.java
@@ -2,6 +2,8 @@ package com.Project2Backend.dao;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +15,7 @@ public interface ImageDao extends JpaRepository<ImageEntity, Integer> {
 	
 	Optional<ImageEntity> findById(int ImageId);
 	
-	
+	@Transactional
 	Optional<ImageEntity> findByReimbursementId(int reimbursementId);
 	
 }
