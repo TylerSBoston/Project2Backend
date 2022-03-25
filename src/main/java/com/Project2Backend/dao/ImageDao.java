108bd6c2dc17fbbc5f9dd2c68b9cb9439ecf6ce8
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-24 20:44:28 -0400

    only gets 1 pickture

--- src/main/java/com/Project2Backend/dao/ImageDao.java
+++ src/main/java/com/Project2Backend/dao/ImageDao.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.Project2Backend.entities.ImageEntity;
@@ -17,6 +18,7 @@ public interface ImageDao extends JpaRepository<ImageEntity, Integer> {
 	Optional<ImageEntity> findById(int ImageId);
 	
 	@Transactional
+	@Query("select u from ImageEntity u where u.reimbursementId =  ?1 order by count(1)")
 	Optional<ImageEntity> findByReimbursementId(int reimbursementId);
 	
 }
