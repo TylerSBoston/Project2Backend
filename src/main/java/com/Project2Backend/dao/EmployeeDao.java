4a5b36313538ad45c079b0cb4fd02f2154468108
Dluck13 <dimitri.luck@revature.net>
2022-03-25 13:35:35 -0400

    added fetchallemployees

--- src/main/java/com/Project2Backend/dao/EmployeeDao.java
+++ src/main/java/com/Project2Backend/dao/EmployeeDao.java
@@ -9,9 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.Project2Backend.entities.EmployeeEntity;
-import com.Project2Backend.entities.ReimbursementEntity;
-import com.Project2Backend.pojo.Employee;
-import com.Project2Backend.pojo.Reimbursement;
+
 
 
 
@@ -22,6 +20,7 @@ import com.Project2Backend.pojo.Reimbursement;
 public interface EmployeeDao extends JpaRepository<EmployeeEntity, Integer>  {
 
 	Optional<EmployeeEntity> findById(int employeeId);
+	
 	
 	@Query(value = "select * from employees where user_name = ?1 and ?2 = user_password",nativeQuery = true)
 	EmployeeEntity login(String username, String password);
