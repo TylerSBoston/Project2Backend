e08e5b28aa1b7a7e9bb7a96598b8b34705a4042f
Dluck13 <dimitri.luck@revature.net>
2022-03-26 18:56:59 -0400

    d

--- src/main/java/com/Project2Backend/controller/ReimbursementController.java
+++ src/main/java/com/Project2Backend/controller/ReimbursementController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -67,7 +69,8 @@ public class ReimbursementController {
 	
 	@Transactional(readOnly = true)
 	@PostMapping(value = "/reimbursements")
-	public ReimbursementEntity submitRequest(@Validated @RequestBody ReimbursementEntity reimbursementEntity) throws SystemException {
+	@ResponseStatus(HttpStatus.CREATED)
+	public ReimbursementEntity submitRequest(@RequestBody ReimbursementEntity reimbursementEntity) throws SystemException {
 		return reimbursementDao.save(reimbursementEntity);
 			
 	}
